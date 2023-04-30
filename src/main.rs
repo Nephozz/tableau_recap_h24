72cc500946273dd47359f1dea64147e44606ccd1
Thomas Bocandé <thomasbocande@gmail.com>
2023-04-30 21:25:56 +0200

    Lecture du tableau des respos clubs et création de celui pour les membres

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,32 @@
-use calamine::{Reader, open_workbook, Xlsx, DataType};
+use calamine::{Reader, open_workbook, Xlsx};
+use rust_xlsxwriter::{Format, Image, Workbook, FormatAlign, FormatBorder, Worksheet};
 
 fn main() {
-let path: &'static str = "C:/Users/thoma/OneDrive/Documents/Internet/H24/05-2023 (réponses).xlsx";
-let mut workbook: Xlsx<_> = open_workbook(path).expect("Ne peux pas ouvrir le fichier !");
+    let path: &'static str = "C:/Users/thoma/OneDrive/Documents/Internet/H24/05-2023 (réponses).xlsx";
+    let reponses: Xlsx<_> = open_workbook(path).expect("Ne peux pas ouvrir le fichier !");
 
-let sheets = workbook.sheet_names();
-let mut i: i32 = 0;
+    let sheets_bind: Vec<String> = reponses.sheet_names().to_owned();
+    let sheets: Vec<&String> = sheets_bind
+        .iter()
+        .filter(|&s| s.contains("TVn7"))
+        .collect();
 
-for s in sheets {
-    if s.contains("TVn7") {
-        i += 1
+    let mois= "Mai";
+
+    let mut workbook = Workbook::new();
+
+    let mut local = workbook.add_worksheet()
+        .set_name("Local");
+
+    let mut b00 = workbook.add_worksheet()
+        .set_name("B00");
+
+    let mut personnes = workbook.add_worksheet()
+        .set_name("Perssonnes avec accès");
+
+    for s in sheets {
+        println!("{}", s);
     }
-}
 
-println!("{}",i);
+    workbook.save("Accès.xlsx");
 }
