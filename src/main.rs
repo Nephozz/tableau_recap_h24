c4e6e74f4c65224512be6f2839a9566d40dcd8ca
Thomas Bocandé <thomasbocande@gmail.com>
2023-04-30 18:14:58 +0200

    Le programme compte désormais le nombre de pages concernant les accès de TVn7

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use calamine::{Reader, open_workbook, Xlsx, DataType};
+
 fn main() {
-    println!("Hello, world!");
+let path: &'static str = "C:/Users/thoma/OneDrive/Documents/Internet/H24/05-2023 (réponses).xlsx";
+let mut workbook: Xlsx<_> = open_workbook(path).expect("Ne peux pas ouvrir le fichier !");
+
+let sheets = workbook.sheet_names();
+let mut i: i32 = 0;
+
+for s in sheets {
+    if s.contains("TVn7") {
+        i += 1
+    }
+}
+
+println!("{}",i);
 }
