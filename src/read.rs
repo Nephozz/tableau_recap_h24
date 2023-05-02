29f35bafd3fc9380f84c05610f556bd11c24dcbe
Thomas Bocandé <thomasbocande@gmail.com>
2023-05-03 00:50:36 +0200

    Réecriture des fonctions read

--- src/read.rs
+++ src/read.rs
@@ -2,13 +2,14 @@ use std::{io::BufReader, fs::File};
 use calamine::{Reader, Xlsx, DataType, Range};
 
 #[derive(Debug)]
-enum Info {
+pub enum Info {
     Date(Vec<String>),
     B00(bool),
-    Personne(Vec<String>),
+    //Personne(Vec<String>),
 }
 
-pub fn read_date(range: &Range<DataType>, i: u32, j: u32, dates_sheet: &mut Vec<String>) {
+pub fn read_date(range: &Range<DataType>, i: u32, j: u32) -> Vec<String> {
+        let mut dates_sheet: Vec<String> = Vec::new();
         let date = range.get_value((i,j))
             .unwrap()
             .to_owned()
@@ -16,9 +17,12 @@ pub fn read_date(range: &Range<DataType>, i: u32, j: u32, dates_sheet: &mut Vec<
         let date_parse: Vec<&str> = date.split_whitespace().collect();
         dates_sheet.push(String::from(date_parse[1]));
         dates_sheet.push(String::from(date_parse[3]));
+
+        return dates_sheet;
 }
 
-pub fn read_peoples(range: &Range<DataType>, personnes_sheet: &mut Vec<String>) {
+pub fn read_peoples(range: &Range<DataType>) -> Vec<String> {
+    let mut personnes_sheet : Vec<String> = Vec::new();
     for i in 4..11 {
         for j in 7..9 {
             let personnes_bind = range.get_value((i,j));
@@ -34,22 +38,20 @@ pub fn read_peoples(range: &Range<DataType>, personnes_sheet: &mut Vec<String>) 
             };
         }
     }
-    println!("{:?}", personnes_sheet);
+    return personnes_sheet;
 }
 
-pub fn read_sheet(workbook: &mut Xlsx<BufReader<File>>, sheets: &Vec<&String>) {
+pub fn read_sheet(workbook: &mut Xlsx<BufReader<File>>, sheets: &Vec<&String>) -> (Vec<Info>,Vec<Vec<String>>) {
     let mut info: Vec<Info> = Vec::new();
+    let mut personnes: Vec<Vec<String>> = Vec::new();
 
     for s in sheets {
-        let b00_sheet: bool;
-        let mut dates_sheet: Vec<String> = Vec::new();
-        let mut personnes_sheet: Vec<String> = Vec::new();
-
         let range: Range<DataType> = workbook.worksheet_range(s).unwrap().unwrap();
 
-        read_date(&range, 1, 3, &mut dates_sheet);
-        read_date(&range, 1, 4, &mut dates_sheet);
+        let mut dates_sheet: Vec<String> = read_date(&range, 1, 3);
+        dates_sheet.append(&mut read_date(&range, 1, 4));
 
+        let b00_sheet: bool;
         let b00: String = range.get_value((1,7))
             .unwrap()
             .to_owned()
@@ -58,10 +60,11 @@ pub fn read_sheet(workbook: &mut Xlsx<BufReader<File>>, sheets: &Vec<&String>) {
             b00_sheet = true;
         } else { b00_sheet = false; };
 
-        read_peoples(&range, &mut personnes_sheet);
+        let personnes_sheet: Vec<String> = read_peoples(&range);
 
         info.push(Info::B00(b00_sheet));
         info.push(Info::Date(dates_sheet));
-        info.push(Info::Personne(personnes_sheet));
+        personnes.push(personnes_sheet);
     }
+    return (info, personnes);
 }
